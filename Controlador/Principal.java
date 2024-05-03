d3748ef2056edac66e5bb02c0441da94a3b08520
AARON SALAZAR MATA <aaron.salazar.mata@gmail.com>
2024-05-03 01:05:30 -0600

    Quitar error en controlador

--- Controlador/Principal.java
+++ Controlador/Principal.java
@@ -1,26 +1,13 @@
 package Controlador;
 
-<<<<<<< HEAD
 import Vista.MenuPrimeraVista;
 
 public class Principal {
 
-    public static void main(String[] args){
-
-        MenuPrimeraVista instancia = new MenuPrimeraVista ();
-        instancia.setVisible (true);
-}
-=======
-import Vista.VentanaOpcionesAdministrativo;
-
-public class Principal {
-
     public static void main(String[] args) {
-        
-        VentanaOpcionesAdministrativo ventanaOpcionesAdministrativo = new VentanaOpcionesAdministrativo();
-        ventanaOpcionesAdministrativo.setVisible(true);
 
+        MenuPrimeraVista instancia = new MenuPrimeraVista();
+        instancia.setVisible(true);
     }
 
->>>>>>> 3cad0a9b1ebb20039fa29b3debb422f8d6be425f
 }
