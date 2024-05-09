06dcaeab0df5c5fc856a313f630ee4ca97d1a15b
AARON SALAZAR MATA <aaron.salazar.mata@gmail.com>
2024-05-08 23:55:41 -0600

    cambios

--- Vista/MenuPrimeraVista.java
+++ Vista/MenuPrimeraVista.java
@@ -109,11 +109,6 @@ public class MenuPrimeraVista extends JFrame implements ActionListener {
             instanciaPaciente.setVisible(true);
             this.dispose();
 
-<<<<<<< HEAD
-        
-        if (e.getSource() == botonSalir){
-
-=======
         } else if (e.getSource() == botonSalir) {
 
             int confirmacion = JOptionPane.showConfirmDialog(null,
@@ -131,7 +126,6 @@ public class MenuPrimeraVista extends JFrame implements ActionListener {
 
             ResultadosPaciente resultadosPaciente = new ResultadosPaciente();
             resultadosPaciente.setVisible(true);
->>>>>>> 9b71d50daaade5d3bdd142b9a315788f1e87fb2d
             this.dispose();
         }
 
@@ -141,4 +135,4 @@ public class MenuPrimeraVista extends JFrame implements ActionListener {
     }
 
     }
-}
+
